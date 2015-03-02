f6362e2c2675dd287da633f4674348c28e19782a
Josiah DeVizia <jsd3828@rit.edu>
2015-03-02 09:40:37 -0500

    Input if-statements and a public static player variable in WorldManager

--- WorldManager.cs
+++ WorldManager.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TheChicagoProject.Entity;
 
 namespace TheChicagoProject
 {
+
     /// <summary>
     /// Manages the worlds (maps) of the game.
     /// </summary>
     class WorldManager
     {
+        public static Player player;
         //test
     }
 }
