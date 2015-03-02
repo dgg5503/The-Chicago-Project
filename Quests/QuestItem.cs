9d8a7ad7c4ddd9ade865f525da06b1d1c9af7732
smlevorse <smlevorse@msn.com>
2015-03-02 10:46:28 -0500

    Added QuestItem class

---
+++ Quests/QuestItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheChicagoProject.Quests
+{
+    /// <summary>
+    /// An item that may be important to the quest
+    /// </summary>
+    class QuestItem
+    {
+        //fields
+        private string name;
+
+        //properties
+        public string Name { get { return name; } }
+
+        //constructor
+        public QuestItem(string name)
+        {
+            this.name = name;
+        }
+    }
+}
