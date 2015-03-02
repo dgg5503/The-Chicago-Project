a07c0add7c540dda74d769b30f3d0b4356ebdcb0
Josiah DeVizia <jsd3828@rit.edu>
2015-03-02 10:27:02 -0500

    Almost fully implemented HandleInput, and added a Reload Method to the Player class

--- InputManager.cs
+++ InputManager.cs
@@ -43,59 +43,67 @@ namespace TheChicagoProject
             }
             if (keyState.IsKeyDown(Keys.E))    //Interact
             {
-
+                //TODO: add implementation of interaction
             }
             if (keyState.IsKeyDown(Keys.R))    //Reload
             {
-
+                WorldManager.player.Reload();
             }
             if (keyState.IsKeyDown(Keys.I))    //Inventory
             {
-
+                Game1.state = GameState.Inventory;
             }
             if (keyState.IsKeyDown(Keys.G))    //Quest log
             {
-
+                Game1.state = GameState.QuestLog;
             }
+            if (keyState.IsKeyDown(Keys.Escape))    //Quest log
+            {
+                if (Game1.state == GameState.Game)
+                    Game1.state = GameState.Pause;
+                else
+                    Game1.state = GameState.Game;
+            }
+
             if (keyState.IsKeyDown(Keys.D1))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 1;
             }
             if (keyState.IsKeyDown(Keys.D2))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 2;
             }
             if (keyState.IsKeyDown(Keys.D3))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 3;
             }
             if (keyState.IsKeyDown(Keys.D4))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 4;
             }
             if (keyState.IsKeyDown(Keys.D5))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 5;
             }
             if (keyState.IsKeyDown(Keys.D6))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 6;
             }
             if (keyState.IsKeyDown(Keys.D7))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 7;
             }
             if (keyState.IsKeyDown(Keys.D8))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 8;
             }
             if (keyState.IsKeyDown(Keys.D9))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 9;
             }
             if (keyState.IsKeyDown(Keys.D0))    //Quick Weapon Select
             {
-
+                WorldManager.player.ActiveWeapon = 0;
             }
 
             //handles mouse input
