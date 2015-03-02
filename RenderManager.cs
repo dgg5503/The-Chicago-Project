91950873a9bb3d446f7a1e21013452111b5b117b
Douglas <dgg5503@rit.edu>
2015-03-01 21:59:16 -0500

    TILES :D!

--- RenderManager.cs
+++ RenderManager.cs
@@ -5,6 +5,67 @@ using System.Text;
 
 namespace TheChicagoProject
 {
+    /*
+     * Tile ideas:
+     * 
+     * (OUT SIDE STUFF)
+     * Notes:
+     * - Top left, Top right, Bottom left, Bottom right tiles will be designated
+     * 
+     * 
+     * Roads
+     *      Line
+     *      Storm Drain
+     *      Tar w/ trolley track
+     *      Tar cracked
+     *      Gravel Stone
+     *      Tar (blank space w/o a line)
+     *      
+     * Sidewalk
+     *      Concrete
+     *      Brick  
+     *      
+     * Lamp
+     *      LampA
+     *      LampB
+     * 
+     * Poles (WIRES)
+     *      (JUST ONE POLE)     
+     * 
+     * Alley
+     *  
+     * Shrubbery
+     *      PlantA
+     *      PlantB
+     *      PlantC
+     * 
+     * Tree
+     *      Pine
+     *      Oak
+     *      Mixed Trees
+     *      
+     * Bridge
+     *      Bridge Edge (rail)
+     *      Bridge middle
+     *      
+     * Buildings (VARY BY COLOR)
+     *      Edge (defined edge of building)
+     *      Roof
+     *      Awnings
+     *      Antennas
+     *      Notes:
+     *          - There will be decorative building tops and edges for special buildings
+     *          
+     * Monument
+     *      Base (base of monument)
+     *      Decorative (the monument itself)
+     *      Obelisk?
+     *      
+     * 
+     * 
+     */
+
+
     /// <summary>
     /// Handles the drawing. Any and all drawing. Ever.
     /// </summary>
