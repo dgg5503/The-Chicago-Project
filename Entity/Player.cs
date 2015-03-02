0538adb8fe433b3a1fb20c69042c9f364301d329
Josiah DeVizia <jsd3828@rit.edu>
2015-03-02 09:59:22 -0500

    Player Class

--- Entity/Player.cs
+++ Entity/Player.cs
@@ -7,5 +7,15 @@ namespace TheChicagoProject.Entity
 {
     class Player : Entity
     {
+
+
+        /// <summary>
+        /// Tells the player to attack
+        /// </summary>
+        /// <param name="type">The type of attack, 0 is primary, 1 is secondary</param>
+        public void Attack(int type)
+        {
+
+        }
     }
 }
