0ad1943dda5ac1f39fe019433ccc936002f704b0
Douglas <dgg5503@rit.edu>
2015-03-02 09:43:25 -0500

    help

--- RenderManager.cs
+++ RenderManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TheChicagoProject
 {
@@ -71,6 +74,22 @@ namespace TheChicagoProject
     /// </summary>
     class RenderManager
     {
+        // Fields
+
+        // The main SpriteBatch taken from Game1 which will
+        // be used to execute the draw method, doing it
+        // this way will require this class to be
+        // constructed in LoadContent (which only happens once)
+        private SpriteBatch sb;
+
+        /// <summary>
+        /// Constructs RenderManager using a SpriteBatch object which will be used for drawing.
+        /// </summary>
+        /// <param name="sb">MonoGames SpriteBatch object.</param>
+        public RenderManager(SpriteBatch sb)
+        {
+            this.sb = sb;
+        }
 
         /// <summary>
         /// Draws shit.
@@ -78,5 +97,22 @@ namespace TheChicagoProject
         public void Draw() {
 
         }
+
+        // Sprite Drawing
+        // This will take loaded content from somewhere 
+        
+
+        // GUI Drawing
+
+
+        // World drawing
+        // takes 2x2 array with tile codes and draws their respective tile from 0,0
+        public void DrawWorld()
+        {
+            // 2x2 
+        }
+
+
+
     }
 }
---
+++ Tile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheChicagoProject
+{
+    enum TileType
+    {
+
+    }
+
+    class Tile
+    {
+        /// <summary>
+        /// Temporary Tile class, just used for DrawWorld testing.
+        /// </summary>
+        public class Tile()
+        {
+
+        }
+    }
+}
