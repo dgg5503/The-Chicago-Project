234cfded4e7b8bc80e62c29539054380889bcff4
Josiah DeVizia <jsd3828@rit.edu>
2015-03-02 10:11:58 -0500

    Updated Player class fields and methods

--- Entity/Player.cs
+++ Entity/Player.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TheChicagoProject.Item;
 
 namespace TheChicagoProject.Entity
 {
     class Player : Entity
     {
+        public Weapon[] holster;
+        public int activeWeapon;
+
+        /// <summary>
+        /// Attacks
+        /// </summary>
+        /// <param name="type">0 is primary fire, 1 is secondary</param>
+        public void Attack(int type)
+        {
+
+        }
     }
 }
