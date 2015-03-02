862bbe09a3135717347890d47928cf30ecb5b3d2
Ashwin Ganapathiraju <ag4ce@hotmail.com>
2015-03-02 09:31:44 -0500

    Added a player class with an inbuilt vector. No one touch Dijkstra map, it's not even close to being finished ATM.

---
+++ Math/DijkstraMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheChicagoProject.Math
+{
+    /// <summary>
+    /// It's not the mathematical DijkstraMap, but rather an AI construct.
+    /// Premise will be explained here at a later date.
+    /// </summary>
+    class DijkstraMap
+    {
+        private static int[][] generateMap(int mapSize, params int[][] goals) {
+            int[][] grid = new int[mapSize][];
+            //Initalizes the grid with 100
+            for (int x = 0; x < grid.Length; x++) {
+                grid[x] = new int[mapSize];
+                for (int y = 0; y < grid.Length; y++)
+                    grid[x][y] = 100;
+            }
+            //Sets the goal points to 0
+            foreach (int[] g in goals) {
+                grid[g[0]][g[1]] = 0;
+            }
+
+            bool b = false;
+            while (!b) {
+                b = true;
+                
+            }
+
+            return grid;
+        }
+    }
+}
