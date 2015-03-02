7949489898f38692b46c3e22fc9766aefffa3a60
Douglas <dgg5503@rit.edu>
2015-03-02 16:57:45 -0500

    Added a missing critical file.

---
+++ GUI/Tiles.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace TheChicagoProject
+{
+    static class Tiles
+    {
+        // Dicitionary of tiles and their file name.
+        public static Dictionary<string, Tile> tilesDictionary = new Dictionary<string, Tile>
+        {
+            {"RoadTar", new Tile(true, "RoadTar.png")},
+
+            {"RoadLine", new Tile(true, "RoadLine.png")},
+
+            {"SideWalkBrick", new Tile(true, "SideWalkBrick.png")},
+
+            {"BuildingEdge", new Tile(false, "BuildingEdge.png")},
+
+            {"BuildingRoof", new Tile(true, "BuildingRoof.png")}
+        };
+
+
+        // POSSIBLE IDEA BELOW?
+
+        // Generate list of tiles from the Content/tiles folder
+        /*
+        public static void LoadTiles()
+        {
+            // WORKING DIRECTIORY STRINGS? (?)
+            string tilesDir = "./Content/Tiles";
+
+            // See if content/tiles exists.
+            if (!Directory.Exists(tilesDir))
+                throw new Exception("Error: Tiles folder not found.");
+
+            // Get all png files in this directory.
+            string[] tilePNGs = Directory.GetFiles(tilesDir, "*?.png");
+
+            // Take each tilePNG file name and make a new tile in the dictionary
+            // the string that identifies the file in the dicitionary is the name
+            // of the tile.
+            for(int i = 0; i < tilePNGs.Length; i++)
+            {
+                tiles.Add(tilePNGs, new Tile()) // HOW TO KNOWW IF WALKABLE???
+            }
+
+        }
+        */
+    }
+}
