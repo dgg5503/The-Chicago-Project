59d1f033aa5f2a288b0394bd56879769e772a2f1
Douglas <dgg5503@rit.edu>
2015-03-02 10:43:17 -0500

    Tiles, no locaiton!

--- Tile.cs
+++ Tile.cs
@@ -23,7 +23,6 @@ namespace TheChicagoProject
     /*
      * Tile Notes:
      * - All tiles will be 64x64 in size.
-     * - X and Y are defined by the upper left side corner
      * - By default, isWalkable is set to true.
      * 
      * 
@@ -40,9 +39,6 @@ namespace TheChicagoProject
         // The tile's texture
         private Texture2D texture;
 
-        // Tile's location and size
-        private Rectangle sizeLoc;
-
         // Bool for whether or not it can be walked on (ASHWIN'S HOLY GRAIL)
         private bool isWalkable;
 
@@ -50,31 +46,11 @@ namespace TheChicagoProject
         private TileType tileType;
 
         // Const width/height
-        private const int SIDE_LENGTH = 64;
+        //private const int SIDE_LENGTH = 64;
 
         // Properties
         // These are self explanatory, I dont think I have to comment what each one of these do...
         public Texture2D Texture { get { return texture; } set { texture = value; } }
-        /// <summary>
-        /// X pixel based location.
-        /// </summary>
-        public int X { get { return sizeLoc.X; } }
-        /// <summary>
-        /// Y pixel based location.
-        /// </summary>
-        public int Y { get { return sizeLoc.Y; } }
-
-        /// <summary>
-        /// Location relative to the side_length. i.e. X / SIDE_LENGTH
-        /// </summary>
-        public int TileX { get { return sizeLoc.X / SIDE_LENGTH; } }
-
-        /// <summary>
-        /// Location relative to the side_length. i.e. Y / SIDE_LENGTH
-        /// </summary>
-        public int TileY { get { return sizeLoc.Y / SIDE_LENGTH;  } }
-
-        public Rectangle SizeLoc { get { return sizeLoc; } }
 
         public bool IsWalkable { get { return isWalkable; } set { isWalkable = value; } }
 
@@ -84,14 +60,12 @@ namespace TheChicagoProject
         /// <summary>
         /// Creates a tile.
         /// </summary>
-        /// <param name="x">X location of tile in pixels.</param>
-        /// <param name="y">Y location of tile in pixels.</param>
         /// <param name="texture">Texture of the tile.</param>
         /// <param name="tileType">The type of tile you wish to create.</param>
-        public Tile(int x, int y, Texture2D texture, TileType tileType)
+        /// <param name="isWalkable">Whether or not the tile can be walked on.</param>
+        public Tile(Texture2D texture, TileType tileType, bool isWalkable)
         {
-            isWalkable = true;
-            this.sizeLoc = new Rectangle(x, y, SIDE_LENGTH, SIDE_LENGTH);
+            this.isWalkable = isWalkable;
             this.texture = texture;
             this.tileType = tileType;
         }
@@ -100,7 +74,7 @@ namespace TheChicagoProject
         /// Basic draw method to draw the tile.
         /// </summary>
         /// <param name="sb">SpriteBatch from Game1.</param>
-        public void Draw(SpriteBatch sb)
+        public void Draw(SpriteBatch sb, Rectangle sizeLoc)
         {
             sb.Draw(texture, sizeLoc, Color.White); //(1)
         }
