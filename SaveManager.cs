e385f9c839e53255d00dccac56b41397ae7e9368
smlevorse <smlevorse@msn.com>
2015-03-04 10:36:56 -0500

    Save Quests completed

--- SaveManager.cs
+++ SaveManager.cs
@@ -13,10 +13,46 @@ namespace TheChicagoProject
     /// </summary>
     class SaveManager
     {
+        public const string QUEST_DIRECTORY = "Quests/";
 
-        public void SaveQuests()
+        /// <summary>
+        /// Saves all of the quests in the quest log
+        /// </summary>
+        /// <param name="log">The player's quest log</param>
+        public void SaveQuests(QuestLog log)
         {
-
+            //Loops through the quest log and save each quest
+            foreach(Quest quest in log.GetLog())
+            {
+                string filePath = QUEST_DIRECTORY + quest.Name + ".quest";
+                SaveQuest(quest, filePath);
+            }
+   
+            /*****************************************************
+             *                                                   *
+             *           Display Completion Message?             *
+             *                                                   *
+             *****************************************************/
         }
+
+        /// <summary>
+        /// Saves a specific quest
+        /// </summary>
+        /// <param name="quest">The Quest</param>
+        /// <param name="path">Where the quest should be stored</param>
+        protected void SaveQuest(Quest quest, string path)
+        {
+            using (StreamWriter output = new StreamWriter(path))
+            {
+                output.WriteLine(quest.Name);
+                output.WriteLine(quest.Description);
+                output.WriteLine(quest.Objective);
+                output.WriteLine(quest.StartPoint.X + ", " + quest.StartPoint.Y);
+                output.WriteLine(quest.Status);
+                output.WriteLine(quest.Reward);
+                output.WriteLine(quest.CashReward);
+            }
+        }
+
     }
 }
