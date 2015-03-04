52d3e02b200518b3c097cb646c9e8bd1da15a66b
Ashwin Ganapathiraju <ag4ce@hotmail.com>
2015-03-04 10:29:44 -0500

    Entities and AI! Just a shell, but it's a start.

---
+++ Entity/LivingEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TheChicagoProject.Item;
+using TheChicagoProject.AI;
+using Microsoft.Xna.Framework;
+
+namespace TheChicagoProject.Entity
+{
+    class LivingEntity : Entity
+    {
+        public List<Item.Item> inventory;
+        public readonly AI.AI entityAI;
+
+        public LivingEntity(AI.AI ai, int x, int y) {
+            this.entityAI = ai;
+            this.location = new Vector2(x, y);
+            inventory = new List<Item.Item>();
+        }
+    }
+}
