de0626e42865685aea7cbd0eadbbc18e4810681f
jsd3828 <jsd3828@rit.edu>
2015-03-04 10:43:55 -0500

    Added fields to the Item class

--- Item/Item.cs
+++ Item/Item.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace TheChicagoProject.Item
 {
     class Item
     {
+        public string name;
+        public Texture2D image;
     }
 }
