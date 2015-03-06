2c74b8c0485746c9be3491d6a76c66e90e5c7f68
jsd3828 <jsd3828@rit.edu>
2015-03-06 09:44:34 -0500

    Updated The Weapon Class

--- Item/Weapon.cs
+++ Item/Weapon.cs
@@ -11,6 +11,8 @@ namespace TheChicagoProject.Item
         private int damage;
         private double reloadTime;
         private string name;
+        private int maxClip;
+        private int loadedAmmo;
 
         /// <summary>
         /// Creates a new Weapon Object
@@ -19,12 +21,15 @@ namespace TheChicagoProject.Item
         /// <param name="dam">The amount of damage the gun does</param>
         /// <param name="reload">The reload time, between 1 and 4 seconds</param>
         /// <param name="name">The name of the gun</param>
-        public Weapon(int rof, int dam, double reload, string name)
+        /// <param name="maxClip">The amount of ammo in a fully loaded clip</param>
+        public Weapon(int rof, int dam, double reload, string name, int maxClip)
         {
             rateOfFire = rof;
             damage = dam;
             reloadTime = reload;
             this.name = name;
+            this.maxClip = maxClip;
+            loadedAmmo = maxClip;
         }
 
         /// <summary>
@@ -70,8 +75,41 @@ namespace TheChicagoProject.Item
         }
 
         /// <summary>
+        /// Gets the amount of ammo loaded into the gun
+        /// </summary>
+        public int LoadedAmmo { get { return loadedAmmo; } }
+
+        /// <summary>
+        /// Gets the amount of ammo in a loaded clip
+        /// </summary>
+        public int MaxClip { get { return maxClip; } }
+
+        /// <summary>
         /// Gets and sets the name of the string
         /// </summary>
         public string Name { get { return name; } set { name = value; } }
+
+        /// <summary>
+        /// Reloads the gun
+        /// </summary>
+        /// <param name="ammo"></param>
+        /// <returns>The amount of ammmo loaded into the gun</returns>
+        public int Reload(int ammo)
+        {
+            if(ammo >= maxClip)
+            {
+                loadedAmmo = maxClip;
+                return maxClip;
+            }
+            else if(ammo > 0)
+            {
+                loadedAmmo = ammo;
+                return ammo;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
