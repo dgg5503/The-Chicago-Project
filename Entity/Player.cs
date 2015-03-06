fcffd4a75b85d1d04afc991b80d8dc11f5a314cd
jsd3828 <jsd3828@rit.edu>
2015-03-06 10:36:46 -0500

    Updated the firing method and added the Update override

--- Entity/Player.cs
+++ Entity/Player.cs
@@ -3,23 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using TheChicagoProject.Item;
+using Microsoft.Xna.Framework;
 
 namespace TheChicagoProject.Entity
 {
 
     enum Direction
     {
-        Up,
-        UpRight,
-        Right,
-        DownRight,
-        Down,
-        DownLeft,
-        Left,
-        UpLeft
+        Up = 0,
+        UpRight = 1,
+        Right = 2,
+        DownRight = 3,
+        Down = 4,
+        DownLeft = 5,
+        Left = 6,
+        UpLeft = 7
     }
 
-    class Player : Entity
+    class Player : LivingEntity
     {
         public Weapon[] holster;
         private int activeWeapon;
@@ -27,10 +28,12 @@ namespace TheChicagoProject.Entity
         private int questPoints;
         private int lives;
         public Direction direction;
+        private Random rand;
+        GameTime gameTime;
 
         private double lastShot;    //The time of the last time the player shot
 
-        public Player()
+        public Player(Rectangle location) : base(null, location)
         {
             holster = new Weapon[10];
             activeWeapon = 0;
@@ -38,10 +41,20 @@ namespace TheChicagoProject.Entity
             questPoints = 0;
             lives = 4;
             direction = Direction.Up;
+            rand = new Random();
         }
 
         //properties
-        public int Cash { get { return Cash; } set { cash = value;/*maybe implement rule that cash cannot be < 0*/} }
+        public int Cash
+        {
+            get { return Cash; }
+            set
+            {
+                if(value < 0)
+                    cash = value;
+            }
+        }
+
         public int QuestPoints { get { return questPoints; } 
             set 
             { 
@@ -62,7 +75,9 @@ namespace TheChicagoProject.Entity
         {
             if(type == 0)
             {
-
+                double trajectory = rand.NextDouble() * holster[activeWeapon].Accuracy * (double)(rand.Next(2) - 1);
+                trajectory += (double)((int)direction * 45);
+                EntityManager.FireBullet(location.X, location.Y, System.Math.Cos(trajectory), System.Math.Sin(trajectory));
             }
         }
 
@@ -109,6 +124,20 @@ namespace TheChicagoProject.Entity
         {
             int newLives = 4 + (int)(2 * System.Math.Sqrt(qPoints));
             return newLives;
+        }
+
+        /// <summary>
+        /// Gives the player a certain amount of cash
+        /// </summary>
+        /// <param name="amount">Amount of cash given to the player</param>
+        public void AwardCash(int amount)
+        {
+            Cash += amount;
+        }
+
+        public override void Update(GameTime time)
+        {
+            gameTime = time;
         }
     }
 }
