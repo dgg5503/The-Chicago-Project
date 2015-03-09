123f83495424920024daefa35c76814601828006
Douglas <dgg5503@rit.edu>
2015-03-08 20:35:24 -0400

    conflict

--- AI/AI.cs
+++ AI/AI.cs
@@ -11,7 +11,7 @@ namespace TheChicagoProject.AI
     /// Why? Because AI's have different "wants".
     /// Every Living Entity has an AI.
     /// </summary>
-    abstract class AI
+    public abstract class AI
     {
     }
 }
