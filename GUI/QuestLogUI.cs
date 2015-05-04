8a0823f2d2cfe5c6ef5072f6574cd01167969940
Douglas Gliner <dgg5503@rit.edu>
2015-05-04 10:45:36 -0400

    continuing work on quest log

--- GUI/QuestLogUI.cs
+++ GUI/QuestLogUI.cs
@@ -13,10 +13,14 @@ namespace TheChicagoProject.GUI
 {
     class QuestLogUI : Control
     {
+        // Controls to be modified
+        private Label questLogHeaderLabel;
+        private Container questBarsContainer;
+
         // List of quest bars for each quest...
         private List<QuestInfoBarUI> questBars;
         private QuestLog questLog;
-        private Label questLogHeaderLabel;
+        
 
         public bool IsQuestLogLoaded
         {
@@ -36,18 +40,33 @@ namespace TheChicagoProject.GUI
 
             // Header container
             Container headerContainer = new Container();
-            headerContainer.Size = new Vector2(this.Size.X, this.Size.Y / 8);
-            headerContainer.Alignment = ControlAlignment.Center;
+            headerContainer.Size = new Vector2(this.Size.X, 20);
+            headerContainer.Alignment = ControlAlignment.Left;
             headerContainer.parent = this;
             Add(headerContainer);
 
             // Header
             questLogHeaderLabel = new Label();
-            questLogHeaderLabel.Alignment = ControlAlignment.Left;
+            questLogHeaderLabel.Alignment = ControlAlignment.Center;
             questLogHeaderLabel.Text = "QUEST LOG";
             questLogHeaderLabel.Color = Color.Black;
-            questLogHeaderLabel.parent = this;
+            questLogHeaderLabel.parent = headerContainer;
             Add(questLogHeaderLabel);
+
+            // Quest bar container
+            questBarsContainer = new Container();
+            questBarsContainer.Alignment = ControlAlignment.Left;
+            questBarsContainer.Size = new Vector2(this.Size.X / 2, this.Size.Y - headerContainer.Size.Y);
+            questBarsContainer.Location = new Vector2(0, headerContainer.Size.Y);
+            questBarsContainer.parent = this;
+            Add(questBarsContainer);
+
+            // Forward backward button container
+
+            // quest information container
+
+            // quest buttons container.
+
 
 
             // test info bar.
@@ -66,15 +85,31 @@ namespace TheChicagoProject.GUI
             foreach(Quest quest in log)
             {
                 QuestInfoBarUI infoBar = new QuestInfoBarUI(new Vector2(200, 80));
-                infoBar.Location = new Vector2(10, this.Size.Y * count);
+                infoBar.Location = new Vector2(questBarsContainer.Size.X / 2 - infoBar.Size.X / 2, (this.Size.Y * count) + 10);
                 infoBar.Alignment = ControlAlignment.Left;
-                infoBar.parent = this;
-                Add(infoBar);
+                infoBar.parent = questBarsContainer;
+                infoBar.Click += infoBar_Click;
+                questBarsContainer.Add(infoBar);
                 infoBar.Load(quest);
+
                 questBars.Add(infoBar);
                 count++;
             }
             questLog = log; 
         }
+
+        // on click, take quest info from bar and pass to a quest ui.
+        private void infoBar_Click(object sender, EventArgs e)
+        {
+            QuestInfoBarUI infoBar = sender as QuestInfoBarUI;
+            if(infoBar != null)
+            {
+                // create quest UI
+                // load the quest
+                // load visuals (how...)
+            }
+        }
+
+        
     }
 }
