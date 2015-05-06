a598dc53c4801419029fd3a5eefa55abd7bc85e1
Douglas Gliner <dgg5503@rit.edu>
2015-05-06 10:29:18 -0400

    starting polish

---
+++ GUI/DragableMatrixV2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TheChicagoProject.GUI;
+using TheChicagoProject.GUI.Forms;
+
+namespace TheChicagoProject.GUI
+{
+    class DragableMatrixV2 : Control
+    {
+    }
+}
